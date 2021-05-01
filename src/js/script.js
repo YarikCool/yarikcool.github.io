e9e656e7cdef9e84aa5f8a406a49bb114ef1d197
Yaroslav <yarikmaster007@gmail.com>
2021-05-01 21:46:18 +0300

    updated catalogue section - added back cards

--- src/js/script.js
+++ src/js/script.js
@@ -24,4 +24,38 @@ $(document).ready(function () {
       },
     ],
   });
+
+  $("ul.catalogue__tabs").on(
+    "click",
+    "li:not(.catalogue__tab_active)",
+    function () {
+      $(this)
+        .addClass("catalogue__tab_active")
+        .siblings()
+        .removeClass("catalogue__tab_active")
+        .closest("div.catalogue__wrapper")
+        .find("div.catalogue__content")
+        .removeClass("catalogue__content_active")
+        .eq($(this).index())
+        .addClass("catalogue__content_active");
+    }
+  );
+
+  // $("catalogue__item-link").on("click", function () {});
+  function toggleCard(className) {
+    $(className).each(function (i) {
+      $(this).on("click", function (e) {
+        e.preventDefault();
+        $(".catalogue__item-front")
+          .eq(i)
+          .toggleClass("catalogue__item-front_active");
+        $(".catalogue__item-back")
+          .eq(i)
+          .toggleClass("catalogue__item-back_active");
+      });
+    });
+  }
+
+  toggleCard(".catalogue__item-link");
+  toggleCard(".catalogue__item-link-back");
 });
